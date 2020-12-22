7aa221e9939a19a3c915307cb24e4079cb703ba1
MukhtarA <33712120+MukhtarA@users.noreply.github.com>
2020-12-22 22:53:40 +0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component, HostBinding, Inject, OnInit, Renderer2} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2 ) {}
+
+  ngOnInit() {
+    this.renderer.setAttribute(this.document.body, 'class', 'theme-dark')
+  }
+
+  switchMode(isDarkMode: boolean){
+    const hostClass = isDarkMode ? 'theme-dark' : 'theme-light';
+    this.renderer.setAttribute(this.document.body, 'class', hostClass);
+  }
+}
